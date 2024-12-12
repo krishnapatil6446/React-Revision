1c360acfc18cbeb0b581394b758bbd6db0ad8628
Krishna Patil <krishnapatil6446@gmail.com>
2024-12-12 14:46:41 +0530

    Listgroup component

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,4 +1,28 @@
-function ListGroup(){
-    return<h1>List Group</h1>;
+function ListGroup() {
+  let items = [
+    "new york",
+    "San Francisco",
+    "India gate",
+    "Maharashtra",
+    "Baroda",
+  ];
+
+  return (
+    <>
+      <h1>List</h1>
+      {items.length === 0 && <p>No item found</p>}
+      <ul className="list-group">
+        {items.map((item, index) => (
+          <li
+            className="list-group-item"
+            key={item}
+            onClick={() => console.log(item, index)}
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 export default ListGroup;
