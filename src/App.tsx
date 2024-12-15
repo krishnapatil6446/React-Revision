d5d46b4fc5ca6b1a701197c2bb79ac92ccfe9546
Krishna Patil <krishnapatil6446@gmail.com>
2024-12-15 23:33:02 +0530

    added some insertions

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import ListGroup from "./components/ListGroup";
+import NewMessage from "./components/NewMessage";
 
-function App(){
-return <div><ListGroup /></div>
+function App() {
+  return (
+    <div>
+      <ListGroup />
+      <NewMessage></NewMessage>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/NewMessage.tsx
@@ -0,0 +1,8 @@
+function NewMessage() {
+  return (
+    <>
+      <h1>This is NewMessage</h1>
+    </>
+  );
+}
+export default NewMessage;
